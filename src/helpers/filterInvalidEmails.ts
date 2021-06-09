c834ccc8f11657bbceaba2a44bb4d7518741cc5b
Lukasz Zatyka <lukasz.zatyka@gmail.com>
2021-06-09 12:00:40 +0200

    Refactor invalidEmail array method

--- src/helpers/filterInvalidEmails.ts
+++ src/helpers/filterInvalidEmails.ts
@@ -1,29 +1,19 @@
 import formatEmailDomain from './formatEmailDomain';
 
 function filterInvalidEmails(emailDomains: string, commitEmails: string[]): string[] {
-  const allowedDomainsBox: string[] = [];
+  const allowedDomains: string[] = [];
 
   emailDomains
     .replace(/\s/g, '')
     .split(',')
     .forEach((email) => {
       const pushedEmail = formatEmailDomain(email);
-      allowedDomainsBox.push(pushedEmail);
+      allowedDomains.push(pushedEmail);
     });
-  console.log(allowedDomainsBox);
 
-  const validEmails: string[] = [];
-  commitEmails.map((el) =>
-    allowedDomainsBox.map((el2) => {
-      if (el.endsWith(el2)) {
-        return validEmails.push(el);
-      }
-    })
+  const invalidEmails = commitEmails.filter((commitEmail) =>
+    allowedDomains.every((domain) => !commitEmail.endsWith(domain))
   );
-  console.log(validEmails);
-
-  const invalidEmails = commitEmails.filter((email) => !validEmails.includes(email));
-  console.log(invalidEmails);
 
   return invalidEmails;
 }
